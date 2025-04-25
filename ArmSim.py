29b142c25fcb6e0559cf7f3aabd46eb6f07babe5
Dominic Salmieri <dsalmieri@olin.edu>
2025-04-24 21:17:29 -0400

    Viz working (uncommented)

--- ArmSim.py
+++ ArmSim.py
@@ -1,57 +1,62 @@
 """
-File for visualizing the arm simulator
+File for visualizing and simulating 2D robot arms
 """
 
 import tkinter as tk
 import numpy as np
 import math
+from weakref import WeakSet
+from collections.abc import Callable
 
 
-class ArmSim:
+class ArmViz:
     """
-    Arm simulation class for visualizing robot arms, with integration for use
-    of a genetic algorithm to solve the inverse kinematics
+    Arm visualizer class for displaying ArmSims.
     """
 
-    # The size of the canvas
-    width = 1920/2
-    height = 1080/2
+    def __init__(self, width: int = 960, height: int = 540):
+        # The size of the canvas
+        self.width = width
+        self.height = height
 
-    # The tkinter objects
-    root = tk.Tk()
-    canvas = tk.Canvas(root, width=width, height=height)
-    canvas.pack()
+        # Tkinter objects
+        self.root = tk.Tk()
+        self.canvas = tk.Canvas(self.root, width=self.width, height=self.height)
+        self.canvas.pack()
+        # Set up close button properly
+        self.root.protocol("WM_DELETE_WINDOW", self.stop)
 
-    # The list of ArmSim instances
-    arms: list['ArmSim'] = [] # The list of arms ArmSim type in quotes to forward reference
+        # Set for storing ArmSim instances assigned to this vizualiser
+        self.arms: WeakSet['ArmSim'] = WeakSet()
 
-    @classmethod
-    def draw(cls):
+        self.running: bool = False
+        self.callback: Callable[[], None] = lambda: None 
+
+    def draw(self):
         """
-        Redraws all of the arms on the class canvas
+        Redraws all of the arms on the canvas
         """
 
         # Clear the canvas
-        cls.canvas.delete('all')
+        self.canvas.delete('all')
 
         # For every instace of an arm sim
-        for arm in cls.arms:
+        for arm in self.arms:
             points = arm.fk()
 
             # Draw a line for each link
             for i, point in enumerate(points):
                 
                 # Get the position of the previous joint (in display space)
-                x0, y0 = cls.point_transform(points[i-1] if i > 0 else (0.0, 0.0))
+                x0, y0 = self.point_transform(points[i-1] if i > 0 else (0.0, 0.0))
 
                 # Get the position of the current joint (in display space)
-                x1, y1 = cls.point_transform(point)
+                x1, y1 = self.point_transform(point)
 
                 # Draw the line
-                cls.canvas.create_line(x0, y0, x1, y1, fill=arm.color, width=5)
+                self.canvas.create_line(x0, y0, x1, y1, fill=arm.color, width=5)
 
-    @classmethod
-    def point_transform(cls, point: tuple[float, float]) -> tuple[float, float]:
+    def point_transform(self, point: tuple[float, float]) -> tuple[float, float]:
         """
         Transforms a point from simulation space to display space (the pixel location on the class canvas)
 
@@ -64,22 +69,38 @@ class ArmSim:
 
         # Set transformation parameters
         scale = 100
-        x_offset = cls.width/2
-        y_offset = cls.height/2
+        x_offset = self.width/2
+        y_offset = self.height/2
 
         # Return the transformed point. Flip y so that -y is down
         return scale * point[0] + x_offset, -scale * point[1] + y_offset 
 
-    @classmethod
-    def run(cls):
+    def run(self):
+        self.running = True        
+        while self.running:
+            self.update()
+
+    def stop(self):
+        self.running = False
+
+    def update(self):
         """
         Run the main display loop and start the simulation
         """
         
-        cls.draw()
-        cls.root.mainloop()
+        self.callback()
+        self.draw()
+        self.root.update_idletasks()
+        self.root.update()
 
-    def __init__(self, thetas: list[float], link_lengths: list[float], color: str = 'black'):
+
+class ArmSim:
+    """
+    Arm simulation class for computing the kinematics of robot arms, with integration for use
+    of a genetic algorithm to solve the inverse kinematics
+    """
+
+    def __init__(self, viz: ArmViz, thetas: list[float], link_lengths: list[float], color: str = 'black'):
         """
         Initializes an instance of the Arm Simulator
 
@@ -91,7 +112,9 @@ class ArmSim:
         self.link_lengths = link_lengths
         self.color = color
 
-        ArmSim.arms.append(self)
+        # Add self to the list of arms in the visualizer.
+        self.viz = viz
+        self.viz.arms.add(self)
 
     def fk(self):  # -> list[tuple[float, float]]:
         """
@@ -124,10 +147,18 @@ class ArmSim:
         return joint_positions
 
 
-sim = ArmSim([np.pi / 3, np.pi / 2], [1, 1])
-sim = ArmSim([-np.pi / 4, -np.pi / 2], [2, 1], 'blue')
-sim = ArmSim([4 * np.pi / 3, -np.pi / 2, np.pi / 2, -np.pi / 2, 1/6 * np.pi], [0.5, 0.5, 0.5, 0.5, 1], 'green')
+if __name__ == '__main__':
+    import time
 
-print(sim.fk())
+    viz = ArmViz()
 
-ArmSim.run()
+    sim1 = ArmSim(viz, [np.pi / 3, np.pi / 2], [1, 1])
+    sim2 = ArmSim(viz, [-np.pi / 4, -np.pi / 2], [2, 1], 'blue')
+    sim3 = ArmSim(viz, [4 * np.pi / 3, -np.pi / 2, np.pi / 2, -np.pi / 2, 1/6 * np.pi], [0.5, 0.5, 0.5, 0.5, 1], 'green')
+
+    def timestep():
+        sim3.thetas[0] += 0.1
+        time.sleep(0.1)
+    
+    viz.callback = timestep
+    viz.run()
